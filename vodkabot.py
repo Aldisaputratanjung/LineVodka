13eee65a6bb2d89da1ad7f7312db0f2e842f768a
Aldisaputratanjung <32579303+Aldisaputratanjung@users.noreply.github.com>
2017-10-08 01:26:14 -0700

    Update vodkabot.py

--- vodkabot.py
+++ vodkabot.py
@@ -39,7 +39,7 @@ def sendMessage(to, text, contentMetadata={}, contentType=0):
 
 def NOTIFIED_ADD_CONTACT(op):
     try:
-        sendMessage(op.param1, client.getContact(op.param1).displayName + "Thanks for add")
+        sendMessage(op.param1, client.getContact(op.param1).displayName + "Thanks you for adding as a friend do not forget spam like on timeline ")
     except Exception as e:
         print e
         print ("\n\nNOTIFIED_ADD_CONTACT\n\n")
